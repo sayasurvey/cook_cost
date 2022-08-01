2bfa561a1faae54f8079e4791f752b728f312333
ユーザ名 <sayasurvey@yahoo.co.jp>
2022-08-01 19:13:35 +0900

    FIX:CSV生成ロジックをモデルに移管
    CSV作成ロジックはビジネスロジックなのでモデルに移動

--- app/controllers/food_costs_controller.rb
+++ app/controllers/food_costs_controller.rb
@@ -5,17 +5,7 @@ class FoodCostsController < ApplicationController
     respond_to do |format|
       format.html
       format.csv do |_csv|
-        food_costs = FoodCost.all
-        csv_data = CSV.generate do |csv|
-          header = %w[id recipe_id price_id quantity_unit cost note]
-          csv << header
-          food_costs.each do |food_cost|
-            values = [food_cost.id, food_cost.recipe_id, food_cost.price_id, food_cost.quantity_unit, food_cost.cost,
-                      food_cost.note]
-            csv << values
-          end
-        end
-        send_data(csv_data, filename: 'food_costs.csv')
+        send_data(FoodCost.csv_output_data, filename: 'food_costs.csv')
       end
     end
   end
--- app/models/food_cost.rb
+++ app/models/food_cost.rb
@@ -1,4 +1,17 @@
 class FoodCost < ApplicationRecord
   belongs_to :price
   belongs_to :recipe
+
+  def self.csv_output_data
+    food_costs = self.all
+    csv_data = CSV.generate do |csv|
+      header = %w[id recipe_id price_id quantity_unit cost note]
+      csv << header
+      food_costs.each do |food_cost|
+        values = [food_cost.id, food_cost.recipe_id, food_cost.price_id, food_cost.quantity_unit, food_cost.cost,
+                  food_cost.note]
+        csv << values
+      end
+    end
+  end
 end
